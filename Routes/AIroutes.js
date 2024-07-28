663d1d3c8f0cd5d067d4195cf90af3a1f51048bb
Tusharjadon04 <154507824+Tusharjadon04@users.noreply.github.com>
2024-07-28 10:09:53 +0530

    sfwwf

--- Routes/AIroutes.js
+++ Routes/AIroutes.js
@@ -2,5 +2,5 @@ const express = require("express")
 const { modelAi } = require("../Middleware/Aimiddle")
 const AIroute=express.Router()
 
-AIroute.get("/Aidata",modelAi)
+AIroute.put("/Aidata",modelAi)
 module.exports=AIroute;
